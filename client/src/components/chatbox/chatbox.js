390055fa094488c94400a1c1cd3cef5eaaf9aa67
ayushhdwi <ayushhdwi@gmail.com>
2021-11-06 12:47:09 +0530

    some other changes and incomplete buttons added, will improve them later

--- client/src/components/chatbox/chatbox.js
+++ client/src/components/chatbox/chatbox.js
@@ -2,20 +2,18 @@ import React ,{useEffect, useState}  from  "react";
 import "./Chatbox.css";
 import io from "socket.io-client";
 
+// icon imports
+import { FaTimes, FaPaperPlane, FaBan } from "react-icons/fa";
+
 
 let socket;
 
 const CONNECTION_PORT = "http://localhost:8000";
 
-// icon imports
-import { FaTimes, FaPaperPlane, FaBan } from "react-icons/fa";
-
 const ChatBox = ({flagChatbox, toggleChatbox}) => {
-
-
-	const [message,setMessage] = useState("");
+    const [message, setMessage] = useState("");
     const [messageList, setMessageList] = useState([]);
-
+    /*
 	useEffect(() => {
 		socket = io(CONNECTION_PORT);
 	}, [CONNECTION_PORT]);
@@ -33,60 +31,58 @@ const ChatBox = ({flagChatbox, toggleChatbox}) => {
 	const sendMessage = async () => {
         var today = new Date();
         let messageContent = {
-          room: roomID,
-          content: {
-            author: userName,
-            message: message,
-            time:today.getHours()+':'+today.getMinutes(),
-          },
+			room: roomID,
+			content: {
+				author: userName,
+				message: message,
+				time:today.getHours()+':'+today.getMinutes(),
+			},
         };
     
         await socket.emit("send_message", messageContent);
         setMessageList([...messageList, messageContent.content]);
         setMessage("");
-      };
-
+	};
+*/
 
     return (
-	<div id="chatbox" className="disable-sth">
-		<div className="chatbox-head">
-			<div className="user">
-				<span className="chat-pic"></span>
-				<span className="chat-name">
-					Parth Bhardwaj
-				</span>
-			</div>
-			<div className="chat-options">
-				<FaBan className="block"/>
-				<FaTimes className="close-chat" onClick={toggleChatbox}/>
-			</div>
-		</div>
-		<div className="chatbox-body">
-			<div className="sentM msg">
-				Look bro ! I found a pair of shoes that you aaa a a a might like !
-			</div>
-			<div className="recievedM msg">
-				Ohh... Thats great !
-			</div>
-			<div className="sentM msg">
-				Look bro ! I found a pair of shoes that you might like !
-			</div>
-			<div className="recievedM msg">
-				Ohh... Thats great !
-			</div>
-			<div className="recievedM msg">
-				Look bro ! I found a pair of shoes that you might like !
-			</div>
-			<div className="recievedM msg">
-				Ohh... Thats great !
-			</div>
-		</div>
-		<div className="msgbox">
-			<input type="text" name="msg" id="msg-draft" placeholder="Type Message here"/>
-			<FaPaperPlane id="send-msg-btn"/>
-		</div>
-	</div>
-	);
+        <div id="chatbox" className="disable-sth">
+            <div className="chatbox-head">
+                <div className="user">
+                    <span className="chat-pic"></span>
+                    <span className="chat-name">Parth Bhardwaj</span>
+                </div>
+                <div className="chat-options">
+                    <FaBan className="block" />
+                    <FaTimes className="close-chat" onClick={toggleChatbox} />
+                </div>
+            </div>
+            <div className="chatbox-body">
+                <div className="sentM msg">
+                    Look bro ! I found a pair of shoes that you aaa a a a might
+                    like !
+                </div>
+                <div className="recievedM msg">Ohh... Thats great !</div>
+                <div className="sentM msg">
+                    Look bro ! I found a pair of shoes that you might like !
+                </div>
+                <div className="recievedM msg">Ohh... Thats great !</div>
+                <div className="recievedM msg">
+                    Look bro ! I found a pair of shoes that you might like !
+                </div>
+                <div className="recievedM msg">Ohh... Thats great !</div>
+            </div>
+            <div className="msgbox">
+                <input
+                    type="text"
+                    name="msg"
+                    id="msg-draft"
+                    placeholder="Type Message here"
+                />
+                <FaPaperPlane id="send-msg-btn" />
+            </div>
+        </div>
+    );
 };
 
 export default ChatBox;
